22a8c21dd5ccce580c919c258419282383679763
BroHomi <BroHomi@users.noreply.github.com>
2017-07-12 23:24:47 -0400

    Update jdkconfig.sh

--- jdkconfig.sh
+++ jdkconfig.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir -pv /tmp/java
+cd /tmp/java
+download_url='http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" ${download_url}
 
 echo -e '
 export JAVA_HOME=/usr/java/jdk1.8.0_111
